9bb88d4704e88ebfcf57a5493521be40d52998d8
EngrGord <chijokeogbuanya@gmail.com>
2024-03-29 16:52:32 +0100

    local deployment

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -22,7 +22,7 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  await deploy("YourContract", {
+  await deploy("Manager", {
     from: deployer,
     // Contract constructor arguments
     args: [deployer],
@@ -33,12 +33,26 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   });
 
   // Get the deployed contract to interact with it after deploying.
-  const yourContract = await hre.ethers.getContract<Contract>("YourContract", deployer);
-  console.log("👋 Initial greeting:", await yourContract.greeting());
+  const manager = await hre.ethers.getContract<Contract>("Manager", deployer);
+  // console.log("👋 Initial greeting:", await manager);
+
+  await deploy("Registry", {
+    from: deployer,
+    // Contract constructor arguments
+    args: [await manager.getAddress()],
+    log: true,
+    // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
+    // automatically mining the contract deployment transaction. There is no effect on live networks.
+    autoMine: true,
+  });
+
+  // Get the deployed contract to interact with it after deploying.
+  // const registry = await hre.ethers.getContract<Contract>("Registry", deployer);
+  // console.log("👋 Initial greeting:", await registry);
 };
 
 export default deployYourContract;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
 // e.g. yarn deploy --tags YourContract
-deployYourContract.tags = ["YourContract"];
+deployYourContract.tags = ["Manager", "Registry"];
